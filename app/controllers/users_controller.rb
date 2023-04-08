53f5d8f2c0b220344378b14eede5cbd86e518ca3
Saman Batool <samanbatool08@gmail.com>
2023-04-07 21:57:18 -0400

    setup all user actions in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,15 +8,32 @@ end
 
 
 def new
+  user = User.new
 end
 
 def create
+  user = User.new(user_params)
+  if user.save
+    render json: user, status: :created
+  else 
+    render json: user.errors
+  end
 end
 
 def sign_in
+  user = User.find_by(email: params[:email])
+  
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    render json: { message: "Sign in was successful", user: user  }
+  else 
+    render json: { message: "Invalid email or password" }
+  end
 end
 
 def log_out
+  session[:user_id] = nil
+  render json: { message: "You have been logged out successfully"}
 end
 
 
@@ -25,4 +42,5 @@ end
 private 
 
 def user_params
+  params.require(:user).permit(:first_name, :last_name, :email, :password)
 end
---
+++ test/models/journal_note_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class JournalNoteTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
